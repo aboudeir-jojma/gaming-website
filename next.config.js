ead08f004e3a8e5dfd299a87f2445c5eb62d8ccb
aboudeir-jojmagit config --global user.email <aboudeir.jojma@gmail.com>
2025-08-27 09:30:21 +0100

    deploy

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,   // génère un dossier /out
+  images: {
+    // autorise les images distantes depuis picsum.photos
+    domains: ["picsum.photos"],
+    unoptimized: true 
+  }
+};
+
+module.exports = nextConfig;
